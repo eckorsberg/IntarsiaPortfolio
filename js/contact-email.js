037863835e7b3ea0548153bc3464b0d4755920f6
Ed Korsberg <ed.korsberg@gmail.com>
2025-07-03 11:59:28 -0400

    remove email js

--- js/contact-email.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const email = "ed.korsberg" + "@" + "gmail.com";
-  const span = document.getElementById("email");
-  if (span) {
-    span.innerHTML = `<a href="mailto:${email}">${email}</a>`;
-  }
-});
